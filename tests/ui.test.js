e4cca789e0edd13fea24fb9056a76fc7293b1ee7
AneVG <anelia79@gmail.com>
2024-03-10 09:40:15 +0200

    change  text massages

--- tests/ui.test.js
+++ tests/ui.test.js
@@ -7,3 +7,12 @@ test("Verify all Books is visible", async ({ page }) => {
     const isAllBooksLinkVisible =  await allBooksLink.isVisible()
     expect(isAllBooksLinkVisible).toBe(true);
 });
+
+
+test("Verify login Button is visible", async ({ page }) => {
+    await page.goto("http://localhost:3000");
+    await page.waitForSelector("nav.navbar");
+    const loginButton = await page.$('a[href="/catalog"]');
+    const isLoginButtonVisible =  await loginButton.isVisible()
+    expect(isLoginButtonVisible).toBe(true);
+});
